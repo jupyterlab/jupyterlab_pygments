85699f2626ef655d60d6012f22626f0688f97827
martinRenou <martin.renou@gmail.com>
2022-04-04 10:50:01 +0200

    Make jupyterlab_pygments a labextension

--- jupyterlab_pygments/__init__.py
+++ jupyterlab_pygments/__init__.py
@@ -1,6 +1,9 @@
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
+from ._version import __version__  # noqa
+from .style import JupyterStyle  # noqa
 
-from .style import JupyterStyle
-from ._version import __version__
 
+def _jupyter_labextension_paths():
+    return [{
+        "src": "labextension",
+        "dest": "jupyterlab_pygments"
+    }]
