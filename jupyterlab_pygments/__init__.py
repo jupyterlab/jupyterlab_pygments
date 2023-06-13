bf4bb011b98b85ef009de19f345b07634263c06c
Frédéric Collonval <fcollonval@users.noreply.github.com>
2023-06-13 12:05:06 +0200

    Deal with _version missing

--- jupyterlab_pygments/__init__.py
+++ jupyterlab_pygments/__init__.py
@@ -1,4 +1,10 @@
-from ._version import __version__  # noqa
+try:
+    from ._version import __version__  # noqa
+except ImportError:
+    # Fallback when using the package in dev mode without installing
+    # in editable mode with pip. Here this is particularly important
+    # to be able to run the generate_css.py script.
+    __version__ = "dev"
 from .style import JupyterStyle  # noqa
 
 
