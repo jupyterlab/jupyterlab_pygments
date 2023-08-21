1f90deeb7a7c3b76967dead455a6490517bf99da
RRosio <rosio_reyes@yahoo.com>
2023-08-21 12:48:17 -0700

    rebooting to capture updates through extension upgrade script

--- jupyterlab_pygments/__init__.py
+++ jupyterlab_pygments/__init__.py
@@ -1,12 +1,14 @@
 try:
-    from ._version import __version__  # noqa
+    from ._version import __version__
 except ImportError:
     # Fallback when using the package in dev mode without installing
-    # in editable mode with pip. Here this is particularly important
-    # to be able to run the generate_css.py script.
+    # in editable mode with pip. It is highly recommended to install
+    # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
+    import warnings
+    warnings.warn("Importing 'jupyterlab_pygments' outside a proper installation.")
     __version__ = "dev"
-from .style import JupyterStyle  # noqa
 
+from .style import JupyterStyle 
 
 def _jupyter_labextension_paths():
     return [{
